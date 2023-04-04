2ef1b81e8171c674928e06e478aa0012ed5db2f7
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 16:53:52 +0530

    Adding values in Binary tree

--- src/com.Bridgelabz/BinarySearchTree.java
+++ src/com.Bridgelabz/BinarySearchTree.java
@@ -12,4 +12,29 @@ public class BinarySearchTree{
             this.data = data;
         }
 }
+// Uc1 = Ability to create a BST by adding 67 and then adding 40 & 70 - Use INode to create My Binary.
+public static void insertNode(int value) {
+    root = insertNode(root,value);
+}
+
+public static Node insertNode(Node root, int value){ // Adding Nodes in Binary Search Tree
+    if (root == null) {
+        root = new Node(value);
+        return root;
+    }
+    if (value < root.data) {                        // If value is less than root then it will go to left.
+        root.left = insertNode(root.left, value);
+    }
+    else {
+        root.right = insertNode(root.right, value); // If value is greater than root then it will go to right.
+    }
+    return root;
+}
+
+public static void main(String[] args) {
+    insertNode(67);
+    insertNode(40);
+    insertNode(70);
+}
+
 }
