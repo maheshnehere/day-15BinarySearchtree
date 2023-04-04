8a23f23ee741b5dc98a090b423d366141964a7f8
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 17:01:18 +0530

    Search method

--- src/com.Bridgelabz/BinarySearchTree.java
+++ src/com.Bridgelabz/BinarySearchTree.java
@@ -12,34 +12,21 @@ public class BinarySearchTree{
             this.data = data;
         }
 }
-// Uc1 = Ability to create a BST by adding 67 and then adding 40 & 70 - Use INode to create My Binary.
-public static void insertNode(int value) {
-    root = insertNode(root,value);
-}
 
-public static Node insertNode(Node root, int value){ // Adding Nodes in Binary Search Tree
-    if (root == null) {
-        root = new Node(value);
-        return root;
-    }
-    if (value < root.data) {                        // If value is less than root then it will go to left.
-        root.left = insertNode(root.left, value);
-    }
-    else {
-        root.right = insertNode(root.right, value); // If value is greater than root then it will go to right.
-    }
-    return root;
-}
-
-// UC2 = Check if all are added with using size method in Binary Tree.
-public static void inOrder(Node root) { // Checking our nodes are in right position or not by inorder method.
-    if (root == null) {
-        return;
+ // Uc3 = Search if 63 is present or not in the tree.
+ public static boolean searchNode(Node root, int key){
+    if(root == null) {
+        return false;
     }
 
-    inOrder(root.left);
-    System.out.print(root.data + " ");
-    inOrder(root.right);
+    if (root.data > key){
+        return searchNode(root.left, key);
+    }
+    else if (root.data == key){
+        return true;
+    } else {
+        return searchNode(root.right, key);
+    }
 }
 
 public static void main(String[] args) {
@@ -47,6 +34,11 @@ public static void main(String[] args) {
     insertNode(40);
     insertNode(70);
     inOrder(root);
+
+    if(searchNode(root,63)){
+        System.out.println("63 is present");
+    }else
+        System.out.println("63 is not found");
 }
 }
 
