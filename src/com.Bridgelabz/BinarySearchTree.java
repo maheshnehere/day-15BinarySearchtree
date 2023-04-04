2decb8d423bbff3fcb5ef9115c69acb06f5e0bee
maheshnehere <maheshnehere93@gmail.com>
2023-04-04 16:48:26 +0530

    To find 63 is present or Not

---
+++ src/com.Bridgelabz/BinarySearchTree.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz;
+
+public class BinarySearchTree{
+
+    static Node root;
+    static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+        }
+}
+}
